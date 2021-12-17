73323140a034055b973a7078471184af18b3fb8e
Bernardo Bas <bernardobas@gmail.com>
2021-12-17 12:52:04 -0300

    h872: Dot-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h872.mk
+    $(LOCAL_DIR)/dot_h872.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_h872-userdebug \
-    lineage_h872-eng
+    dot_h872-userdebug \
+    dot_h872-user \
+    dot_h872-eng
--- dot_h872.mk
+++ dot_h872.mk
@@ -18,11 +18,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from h872 device
 $(call inherit-product, device/lge/h872/device.mk)
+
+# DotOS variables
+#WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Security patch level
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2018-10-01
@@ -30,7 +34,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h872
-PRODUCT_NAME := lineage_h872
+PRODUCT_NAME := dot_h872
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H872
 PRODUCT_MANUFACTURER := LGE
